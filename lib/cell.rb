571adcf2535d71efb930b09e1eafd141d74f1c25
Sid Swaminathan <sidswaminathan@yahoo.com>
2024-05-24 15:37:57 -0500

    add Cell model

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,4 +1,20 @@
-class Cell
+class Cell    
+    attr_reader :coordinate, :ship
 
-    
+    def initialize(coordinate)
+        @coordinate = coordinate
+        @ship = nil
+    end
+
+    def empty?
+        if (@ship == nil)
+            return true
+        end
+
+        return false
+    end
+
+    def place_ship(ship)
+        @ship = ship
+    end
 end
