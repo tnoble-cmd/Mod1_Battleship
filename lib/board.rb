6e35e210b5f97313b0eaf2739dd53a2ce48aa14f
Sid Swaminathan <sidswaminathan@yahoo.com>
2024-05-24 16:18:13 -0500

    add board class / testing spec

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,32 @@
+require 'cell'
+
 class Board
+    def initialize
+        @cells = {
+            "A1" => Cell.new("A1"),
+            "A2" => Cell.new("A2"),
+            "A3" => Cell.new("A3"),
+            "A4" => Cell.new("A4"),
+            "B1" => Cell.new("B1"),
+            "B2" => Cell.new("B2"),
+            "B3" => Cell.new("B3"),
+            "B4" => Cell.new("B4"),
+            
+            # C
+            "B1" => Cell.new("B1"),
+            "B2" => Cell.new("B2"),
+            "B3" => Cell.new("B3"),
+            "B4" => Cell.new("B4"),
+            
+            # D
+            "B1" => Cell.new("B1"),
+            "B2" => Cell.new("B2"),
+            "B3" => Cell.new("B3"),
+            "B4" => Cell.new("B4"),
+        }
+    end    
 
-
-    
+    def cells
+        return @cells
+    end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,6 +1,14 @@
 require 'spec_helper'
 
 RSpec.describe Board do
+    before(:each) do
+        @board = Board.new()
+    end
 
-
+    describe '#initialize' do
+        it 'can initialize' do
+            expect(@board).to be_an_instance_of(Board)
+            expect(@board.cells).to be_an_instance_of(Hash)
+        end
+    end
 end
