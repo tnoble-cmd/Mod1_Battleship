3cf252661686f936b61ad36a4044e19ed9b8416d
Sid Swaminathan <sidswaminathan@yahoo.com>
2024-05-26 10:16:14 -0500

    add cells instance method to Board class + testing spec

--- lib/board.rb
+++ lib/board.rb
@@ -11,22 +11,24 @@ class Board
             "B2" => Cell.new("B2"),
             "B3" => Cell.new("B3"),
             "B4" => Cell.new("B4"),
-            
-            # C
-            "B1" => Cell.new("B1"),
-            "B2" => Cell.new("B2"),
-            "B3" => Cell.new("B3"),
-            "B4" => Cell.new("B4"),
-            
-            # D
-            "B1" => Cell.new("B1"),
-            "B2" => Cell.new("B2"),
-            "B3" => Cell.new("B3"),
-            "B4" => Cell.new("B4"),
+            "C1" => Cell.new("C1"),
+            "C2" => Cell.new("C2"),
+            "C3" => Cell.new("C3"),
+            "C4" => Cell.new("C4"),
+            "D1" => Cell.new("D1"),
+            "D2" => Cell.new("D2"),
+            "D3" => Cell.new("D3"),
+            "D4" => Cell.new("D4"),
         }
     end    
 
     def cells
         return @cells
     end
+
+    def valid_coordinate?(cell)
+        
+    end
 end
+
+binding.pry
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -9,6 +9,18 @@ RSpec.describe Board do
         it 'can initialize' do
             expect(@board).to be_an_instance_of(Board)
             expect(@board.cells).to be_an_instance_of(Hash)
+            expect(@board.cells.length).to be(16)
+            expect(@board.cells.values[0]).to be_an_instance_of(Cell)
+        end
+    end
+
+    describe '#valid_coordinate?' do
+        it 'can tell us if a coordinate is on the board or not' do
+            expect(@board.valid_coordinate?("A1").to be(true))
+            expect(@board.valid_coordinate?("D4").to be(true))
+            expect(@board.valid_coordinate?("A5").to be(false))
+            expect(@board.valid_coordinate?("E1").to be(false))
+            expect(@board.valid_coordinate?("A22").to be(false))
         end
     end
 end
